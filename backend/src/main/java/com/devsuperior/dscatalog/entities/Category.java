df0cc05bd7fbe5bb0381dfade4aca87386753baf
Levi <dantaslevi01@gmail.com>
2023-09-22 22:29:03 -0300

    Criação da conexão com banco

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -1,12 +1,18 @@
 package com.devsuperior.dscatalog.entities;
 
+import jakarta.persistence.*;
+import org.springframework.context.annotation.EnableMBeanExport;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_category")
 public class Category implements Serializable {
-
     private static final long serivalVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
 
